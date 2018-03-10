ee13cea89e92db9ce9bb4e66c29f57b3bfc950cb
duncan-crane <31994020+duncan-crane@users.noreply.github.com>
2018-03-10 08:23:30 +0000

    Combine with non render new line and wikimagic processing

--- MW_tinymce.js
+++ MW_tinymce.js
@@ -30,192 +30,124 @@ window.mwTinyMCEInit = function( tinyMCESelector ) {
 	});
 */
 
-      window.tinymce.init({ 
+	window.tinymce.init({ 
 //          selector: '.tinymce',
-          selector: tinyMCESelector,
-	  branding: false,
+		selector: tinyMCESelector,
+		branding: false,
 //	  relative_urls: false,
 //	  remove_script_host: false,
-	  document_base_url: mw.config.get( "wgServer" ),
-	  automatic_uploads: true,
-          paste_data_images: true,
-	  content_css: scriptPath + '/extensions/TinyMCE/MW_tinymce.css',
-          theme_url: scriptPath + '/extensions/TinyMCE/tinymce/themes/modern/theme.js',
-          skin_url: scriptPath + '/extensions/TinyMCE/tinymce/skins/lightgray',
-          paste_word_valid_elements: 'b,strong,i,em,h1,h2,h3,h4,h5,table,thead,tfoot,tr,th,td,ol,ul,li,a,sub,sup,strike,br,del,div,p',
-          invalid_elements: 'tbody',
-	  wiki_tags_list: mw.config.get('wgTinyMCETagList'),
-	  additional_wiki_tags: '|ol|ul|li|h1|h2|h3|h4|h5|h6',
-          browser_spellcheck: true,
-	  wikimagic_context_toolbar: true,
-          contextmenu: "undo redo | cut copy paste insert | link wikimagic inserttable | styleselect removeformat ",
-          convert_fonts_to_spans: true,
-	  link_title: false,
-	  link_assume_external_targets: true,
-	  link_class_list: [
- 		{title: 'External', value: 'external bs-external-link mceNonEditable'},
- 		{title: 'Internal', value: 'internal bs-internal-link mceNonEditable'},
-	  ],
-	  visual_table_class : "",
-          table_default_attributes: {
-              class: 'wikitable'
-          },
-          height: 400,
-          statusbar: false,
-	  // the default text direction for the editor
-	  directionality: tinyMCEDirectionality,
-	  // default language
-	  //language: 'en',
-	  language_url: tinyMCELangURL,
-	  // don't wrap the editable element?
-	  nowrap: false,
-	  // enable resizing for element like images, tables or media objects
-	  object_resizing: true,
-	  // the html mode for tag creation (we need xhtml)
-	  element_format: 'xhtml',
-	  // define the element what all inline elements needs to be wrapped in
-	  //forced_root_block: 'div',
-	  //forced_root_block: false,
-	  forced_root_block: 'p',
-	  forced_root_block_attrs: {
-		'class': 'mw_paragraph'
-	  },
-	  // keep current style on pressing return
-	  keep_styles: true,
-	  // save plugin
-	  save_enablewhendirty: true,
-	  //Allow style tags in body and unordered lists in spans (inline)
-	  valid_children: "+span[ul]",
-	  //set the id of the body tag in iframe to bodyContent, so styles do
-	  //apply in a correct manner. This may be dangerous.
-	  body_id: 'bodyContent',
-	  //Allowable file typr for file picker
-  	  file_picker_types: 'file image media', 
-          //Enable/disable options in upload popup
-          image_description: true,
-          image_title: true,
-          image_dimensions: true,
-          image_advtab: true,
-	  image_class_list: [
+		document_base_url: mw.config.get( "wgServer" ),
+		automatic_uploads: true,
+		paste_data_images: true,
+		content_css: scriptPath + '/extensions/TinyMCE/MW_tinymce.css',
+		theme_url: scriptPath + '/extensions/TinyMCE/tinymce/themes/modern/theme.js',
+		skin_url: scriptPath + '/extensions/TinyMCE/tinymce/skins/lightgray',
+		paste_word_valid_elements: 'b,strong,i,em,h1,h2,h3,h4,h5,table,thead,tfoot,tr,th,td,ol,ul,li,a,sub,sup,strike,br,del,div,p',
+		invalid_elements: 'tbody',
+		wiki_non_rendering_newline_charcter: '&para;', // set to false if you don't use non-rendering single new lines in wiki
+		wiki_tags_list: mw.config.get('wgTinyMCETagList'), 
+		additional_wiki_tags: '|ol|ul|li|h1|h2|h3|h4|h5|h6',
+		browser_spellcheck: true,
+		wikimagic_context_toolbar: true,
+		contextmenu: "undo redo | cut copy paste insert | link wikimagic inserttable | styleselect removeformat ",
+		convert_fonts_to_spans: true,
+		link_title: false,
+		link_assume_external_targets: true,
+		link_class_list: [
+			{title: 'External', value: 'external bs-external-link mceNonEditable'},
+			{title: 'Internal', value: 'internal bs-internal-link mceNonEditable'},
+		],
+		visual_table_class : "",
+		table_default_attributes: {
+			class: 'wikitable'
+		},
+		height: 400,
+		statusbar: false,
+		// the default text direction for the editor
+		directionality: tinyMCEDirectionality,
+		// default language
+		//language: 'en',
+		language_url: tinyMCELangURL,
+		// don't wrap the editable element?
+		nowrap: false,
+		// enable resizing for element like images, tables or media objects
+		object_resizing: true,
+		// the html mode for tag creation (we need xhtml)
+		element_format: 'xhtml',
+		// define the element what all inline elements needs to be wrapped in
+		forced_root_block: 'p',
+		forced_root_block_attrs: {
+			'class': 'mw_paragraph'
+		},
+		// keep current style on pressing return
+		keep_styles: true,
+		// save plugin
+		save_enablewhendirty: true,
+		//Allow style tags in body and unordered lists in spans (inline)
+		valid_children: "+span[ul]",
+		//set the id of the body tag in iframe to bodyContent, so styles do
+		//apply in a correct manner. This may be dangerous.
+		body_id: 'bodyContent',
+		//Allowable file typr for file picker
+		file_picker_types: 'file image media', 
+		//Enable/disable options in upload popup
+		image_description: true,
+		image_title: true,
+		image_dimensions: true,
+		image_advtab: true,
+		image_class_list: [
     		{title: mw.msg("tinymce-upload-type-label-file"), value: 'File'},
     		{title: mw.msg("tinymce-upload-type-label-url"), value: 'URL'},
     		{title: mw.msg("tinymce-upload-type-label-wiki"), value: 'Wiki'}
 		],
-          external_plugins: {
-//             'advlist': scriptPath + '/extensions/TinyMCE/tinymce/plugins/advlist/plugin.js',
-             'anchor': scriptPath + '/extensions/TinyMCE/tinymce/plugins/anchor/plugin.js',
-             'autolink': scriptPath + '/extensions/TinyMCE/tinymce/plugins/autolink/plugin.js',
-             'autoresize': scriptPath + '/extensions/TinyMCE/tinymce/plugins/autoresize/plugin.js',
-             'autosave': scriptPath + '/extensions/TinyMCE/tinymce/plugins/autosave/plugin.js',
-             'charmap': scriptPath + '/extensions/TinyMCE/tinymce/plugins/charmap/plugin.js',
-             'colorpicker': scriptPath + '/extensions/TinyMCE/tinymce/plugins/colorpicker/plugin.js',
-             'contextmenu': scriptPath + '/extensions/TinyMCE/tinymce/plugins/contextmenu/plugin.js',
-             'insertdatetime': scriptPath + '/extensions/TinyMCE/tinymce/plugins/insertdatetime/plugin.js',
-             'lists': scriptPath + '/extensions/TinyMCE/tinymce/plugins/lists/plugin.js',
-             'noneditable': scriptPath + '/extensions/TinyMCE/tinymce/plugins/noneditable/plugin.js',
-             'preview': scriptPath + '/extensions/TinyMCE/tinymce/plugins/preview/plugin.js',
-             'save': scriptPath + '/extensions/TinyMCE/tinymce/plugins/save/plugin.js',
-             'searchreplace': scriptPath + '/extensions/TinyMCE/tinymce/plugins/searchreplace/plugin.js',
-             'table': scriptPath + '/extensions/TinyMCE/tinymce/plugins/table/plugin.js',
-             'textcolor': scriptPath + '/extensions/TinyMCE/tinymce/plugins/textcolor/plugin.js',
-             'visualblocks': scriptPath + '/extensions/TinyMCE/tinymce/plugins/visualblocks/plugin.js',
-             'wikicode': scriptPath + '/extensions/TinyMCE/tinymce/plugins/mw_wikicode/plugin.js',
-             'wikiupload': scriptPath + '/extensions/TinyMCE/tinymce/plugins/mw_upload/plugin.js',
-             'wikilink': scriptPath + '/extensions/TinyMCE/tinymce/plugins/mw_link/plugin.js',
-             'wikimagic': scriptPath + '/extensions/TinyMCE/tinymce/plugins/mw_wikimagic/plugin.js',
-             'wikipaste': scriptPath + '/extensions/TinyMCE/tinymce/plugins/mw_paste/plugin.js',
-             'wikisourcecode': scriptPath + '/extensions/TinyMCE/tinymce/plugins/mw_code/plugin.js'
-          },
-          menubar: false, //'edit insert view format table tools',
-          removed_menuitems: 'media',
-          toolbar1: 'undo redo | cut copy paste insert | bold italic underline strikethrough subscript superscript forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | charmap singlelinebreak wikilink unlink table wikiupload wikimagic wikisourcecode | formatselect removeformat | searchreplace ',
-          style_formats_merge: true,
-          style_formats: [
-            {title: "Table", items: [
-                {title: "Sortable", selector: "table", classes: "sortable"},
-                {title: "Wikitable", selector: "table", classes: "wikitable"},
-                {title: "Contenttable", selector: "table", classes: "contenttable"},
-            ]},
-            {title: "Cell", items: [
-                {title: "Left", selector: "td", format: "alignleft", icon: "alignleft"},
-                {title: "Center", selector: "td", format: "aligncenter", icon: "aligncenter"},
-                {title: "Right", selector: "td", format: "alignright", icon: "alignright"},
-                {title: "Align Top", selector: "td", styles: {verticalalign: "top"}},
-                {title: "Align Middle", selector: "td", styles: {verticalalign: "middle"}},
-                {title: "Align Bottom", selector: "td", styles: {verticalalign: "bottom"}}
-            ]},
-            	{title: "Pre", block: "pre", classes: "bs_pre_from_space" },
-            	{title: "Paragraph", block: "p" }
-          ],
-          images_upload_credentials: true,
-          autoresize_max_height: 400,
-          setup: function(editor) {
-
-             	function insertImage() {
-                	var editorid = editor.id;
-			var node = editor.selection.getNode();
-			var nodeID;
-			var uploadType;
-			if (node.nodeName == 'IMG') {
-				uploadType = "local";
-				nodeID = node.id;
-			} else {
-				uploadType = "file";
-				nodeID = '';
-			}
-
-                 	var uploadform = scriptPath + '/index.php?title=Special:TinyMCEUploadWindow&pfInputID=' + editorid + 
-				'&pfEditor=tinymce' + 
-				'&pfSelect=' + editor.selection.getContent() + 
-				'&pfNode=' + nodeID + 
-				'&wpSourceType=' + uploadType ;
-                 	$.fancybox([
-                      		uploadform
-                      		], {
-                         		'padding'           : 10,
-                         		'type'              : 'iframe',//set type to iframe
-                         		'overlayOpacity'    : 0.3,
-                         		'overlayColor'      : 'grey',
-                         		'speedIn'           : 50,
-                         		'speedOut'          : 50,
-                         		'width'             : 680,
-                         		'height'            : 715
-                 	});
-                 	$.fancybox.resize;
-             	}
-
-             	function insertSingleLinebreak() {
-			var slb = " <span class='single_linebreak' title='single linebreak'>&para;</span>  ";
-			args = {format: 'raw'};
-			editor.undoManager.transact(function(){
-				editor.focus();
-				editor.selection.setContent(slb,args);
-				editor.undoManager.add();
-			});
-            	}
-
-/*
-        	editor.addButton('wikiimageupload', {
-                	icon: 'image',
-			stateSelector: 'img',
-                	tooltip: mw.msg("tinymce-upload"),
-                	onclick:  insertImage
-        	});
-*/
-
-		editor.addButton('singlelinebreak', {
-                 	icon: 'visualchars',
-                 	tooltip: mw.msg("tinymce-insert-linebreak"),
-                 	onclick:  insertSingleLinebreak
-		});
-
-		editor.addMenuItem('singlelinebreak', {
-			icon: 'visualchars',
-			text: 'Single linebreak',
-			tooltip: mw.msg("tinymce-insert-linebreak"),
-			context: 'insert',
-			onclick: insertSingleLinebreak
-		});
+		external_plugins: {
+			'anchor': scriptPath + '/extensions/TinyMCE/tinymce/plugins/anchor/plugin.js',
+			'autolink': scriptPath + '/extensions/TinyMCE/tinymce/plugins/autolink/plugin.js',
+			'autoresize': scriptPath + '/extensions/TinyMCE/tinymce/plugins/autoresize/plugin.js',
+			'autosave': scriptPath + '/extensions/TinyMCE/tinymce/plugins/autosave/plugin.js',
+			'charmap': scriptPath + '/extensions/TinyMCE/tinymce/plugins/charmap/plugin.js',
+			'colorpicker': scriptPath + '/extensions/TinyMCE/tinymce/plugins/colorpicker/plugin.js',
+			'contextmenu': scriptPath + '/extensions/TinyMCE/tinymce/plugins/contextmenu/plugin.js',
+			'insertdatetime': scriptPath + '/extensions/TinyMCE/tinymce/plugins/insertdatetime/plugin.js',
+			'lists': scriptPath + '/extensions/TinyMCE/tinymce/plugins/lists/plugin.js',
+			'noneditable': scriptPath + '/extensions/TinyMCE/tinymce/plugins/noneditable/plugin.js',
+			'preview': scriptPath + '/extensions/TinyMCE/tinymce/plugins/preview/plugin.js',
+			'save': scriptPath + '/extensions/TinyMCE/tinymce/plugins/save/plugin.js',
+			'searchreplace': scriptPath + '/extensions/TinyMCE/tinymce/plugins/searchreplace/plugin.js',
+			'table': scriptPath + '/extensions/TinyMCE/tinymce/plugins/table/plugin.js',
+			'textcolor': scriptPath + '/extensions/TinyMCE/tinymce/plugins/textcolor/plugin.js',
+			'visualblocks': scriptPath + '/extensions/TinyMCE/tinymce/plugins/visualblocks/plugin.js',
+			'wikicode': scriptPath + '/extensions/TinyMCE/tinymce/plugins/mw_wikicode/plugin.js',
+			'wikiupload': scriptPath + '/extensions/TinyMCE/tinymce/plugins/mw_upload/plugin.js',
+			'wikilink': scriptPath + '/extensions/TinyMCE/tinymce/plugins/mw_link/plugin.js',
+//			'wikimagic': scriptPath + '/extensions/TinyMCE/tinymce/plugins/mw_wikimagic/plugin.js',
+			'wikipaste': scriptPath + '/extensions/TinyMCE/tinymce/plugins/mw_paste/plugin.js',
+			'wikisourcecode': scriptPath + '/extensions/TinyMCE/tinymce/plugins/mw_code/plugin.js'
+		},
+		menubar: false, //'edit insert view format table tools',
+		removed_menuitems: 'media',
+		toolbar1: 'undo redo | cut copy paste insert | bold italic underline strikethrough subscript superscript forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | charmap singlelinebreak wikilink unlink table wikiupload wikimagic wikisourcecode | formatselect removeformat | searchreplace ',
+		style_formats_merge: true,
+		style_formats: [
+			{title: "Table", items: [
+				{title: "Sortable", selector: "table", classes: "sortable"},
+				{title: "Wikitable", selector: "table", classes: "wikitable"},
+				{title: "Contenttable", selector: "table", classes: "contenttable"},
+			]},
+			{title: "Cell", items: [
+				{title: "Left", selector: "td", format: "alignleft", icon: "alignleft"},
+				{title: "Center", selector: "td", format: "aligncenter", icon: "aligncenter"},
+				{title: "Right", selector: "td", format: "alignright", icon: "alignright"},
+				{title: "Align Top", selector: "td", styles: {verticalalign: "top"}},
+				{title: "Align Middle", selector: "td", styles: {verticalalign: "middle"}},
+				{title: "Align Bottom", selector: "td", styles: {verticalalign: "bottom"}}
+			]},
+				{title: "Pre", block: "pre", classes: "bs_pre_from_space" },
+				{title: "Paragraph", block: "p" }
+		],
+		images_upload_credentials: true,
+		autoresize_max_height: 400,
+		setup: function(editor) {
 
 		var numMacros = tinyMCEMacros.length;
 		for ( var i = 0; i < numMacros; i++ ) {
