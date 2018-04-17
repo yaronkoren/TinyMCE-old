38f9fdda9d2a9440949bbf2ae326657ea6048007
yaron <yaron57@gmail.com>
2018-04-17 16:06:02 +0400

    Added missing "i18n magic" file for __NOTINYMCE__

---
+++ TinyMCE.i18n.magic.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *  Magic words for extension.
+ */
+
+$magicWords = array();
+
+/** English (English) */
+$magicWords['en'] = array(
+	'MAG_NOTINYMCE' => array( 0, '__NOTINYMCE__' ),
+);
